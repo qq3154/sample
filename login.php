e4aa9f67c907526fda07fe37cfb06e6bfe03b0b7
Quân Nguyễn <kjbinben@gmail.com>
2021-03-01 23:33:07 +0700

    change form all

--- login.php
+++ login.php
@@ -1,139 +1,27 @@
 <html>
 	<head>
 		<title>login</title>
-		<style>
-			body {font-family: Arial, Helvetica, sans-serif;}
-
-			/* Full-width input fields */
-			input[type=text], input[type=password] {
-			  width: 100%;
-			  padding: 12px 20px;
-			  margin: 8px 0;
-			  display: inline-block;
-			  border: 1px solid #ccc;
-			  box-sizing: border-box;
-			}
-
-			/* Set a style for all buttons */
-			button {
-			  background-color: #4CAF50;
-			  color: white;
-			  padding: 14px 20px;
-			  margin: 8px 0;
-			  border: none;
-			  cursor: pointer;
-			  width: 100%;
-			}
-
-			button:hover {
-			  opacity: 0.8;
-			}
-
-			/* Extra styles for the cancel button */
-			.cancelbtn {
-			  width: auto;
-			  padding: 10px 18px;
-			  background-color: #f44336;
-			}
-
-			/* Center the image and position the close button */
-			
-
-			
-
-			.container {
-			  border: solid gray 1px;
-			  width: 50%;
-			  border-radius: 5px;
-			  margin: 100px auto;
-			  background: white;
-			}
-
-			span.psw {
-			  float: right;
-			  padding-top: 16px;
-			}
-
-			/* The Modal (background) */
-			.modal {
-			  display: none; /* Hidden by default */
-			  position: fixed; /* Stay in place */
-			  z-index: 1; /* Sit on top */
-			  left: 0;
-			  top: 0;
-			  width: 100%; /* Full width */
-			  height: 100%; /* Full height */
-			  overflow: auto; /* Enable scroll if needed */
-			  background-color: rgb(0,0,0); /* Fallback color */
-			  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-			  padding-top: 60px;
-			}
-
-			/* Modal Content/Box */
-			.modal-content {
-			  background-color: #fefefe;
-			  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-			  border: 1px solid #888;
-			  width: 80%; /* Could be more or less, depending on screen size */
-			}
-
-			/* The Close Button (x) */
-			.close {
-			  position: absolute;
-			  right: 25px;
-			  top: 0;
-			  color: #000;
-			  font-size: 35px;
-			  font-weight: bold;
-			}
-
-			.close:hover,
-			.close:focus {
-			  color: red;
-			  cursor: pointer;
-			}
-
-			/* Add Zoom Animation */
-			.animate {
-			  -webkit-animation: animatezoom 0.6s;
-			  animation: animatezoom 0.6s
-			}
-
-			@-webkit-keyframes animatezoom {
-			  from {-webkit-transform: scale(0)} 
-			  to {-webkit-transform: scale(1)}
-			}
-  
-			@keyframes animatezoom {
-			  from {transform: scale(0)} 
-			  to {transform: scale(1)}
-			}
-
-			/* Change styles for span and cancel button on extra small screens */
-			@media screen and (max-width: 300px) {
-			  span.psw {
-				 display: block;
-				 float: none;
-			  }
-			  .cancelbtn {
-				 width: 100%;
-			  }
-			}
-		</style>
+		<link rel="stylesheet" types"text/css" href="css/login.css"
 	</head>
 	<body>
-		<form action="connect.php" method="post">
-				<div class="container">
-					 <label for="uname"><b>Username</b></label>
-					<input type="text" placeholder="Enter Username" id ="user" name="user" required>
-
-					 <label for="psw"><b>Password</b></label>
-					<input type="password" placeholder="Enter Password" id="pass" name="pass" required>
-
-					<button type="submit" id="btn">Login</button>					
-				</div>
+		<div id="form">
+			<form action="connect.php" method="post">
+				<p>
+					<label>Username:</label>
+					<input> type="text" placeholder="Enter Username" id="user" name="user"/>
+				</p>
+			
+				<p>
+					<label>Password:</label>
+					<input> type="password" placeholder="Enter Password id="pass" name="pass"/>
+				</p>
+			
+				<p>
+					<input type="submit" id="btn" value="Login">
+				</p>		
 			
 			</form>
+		</div>			
 	</body>
 </html>
 
