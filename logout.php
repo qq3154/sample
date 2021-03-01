d486de500bf4762f8fff701d9c1cd01b113fa4d2
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 03:50:19 +0700

    s

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
-	unset($_SESSION['username']);
-	unset($_SESSION['role']);
+	unset($_SESSION["username"]);
+	unset($_SESSION["role"]);
 	session_detroy();
 	echo"<script> window.location.href = '../index.php' ; </script>";
 	exit();
