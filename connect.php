cb7bdbf8ed57cfc733868c28f790657148c3d0c1
Quân Nguyễn <kjbinben@gmail.com>
2021-03-01 23:59:03 +0700

    fix

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+	/*
 	$username = $_POST['user'];
 	$password = $_POST['pass';]
 
@@ -26,5 +27,54 @@
 	{
 		echo "Failed to login!!!";
 	}
+	*/
+	# Heroku credential 
+			$host_heroku = "ec2-34-203-255-149.compute-1.amazonaws.com";
+			$db_heroku = "dfafdda85iuufp";
+			$user_heroku = "fwhvqbdasvcpdw";
+			$pw_heroku = "ee454b013d21e0981d72f13efd5297cb5392457328d856308fe8bd8b23ac168a";
+			
+			# Create connection to Heroku Postgres
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+			# Get data by query
+			$query = 'select * from test_lab6';
+			$result = pg_query($pg_heroku, $query);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
+
+			echo '</table></body></html>';
 ?>
 		
--- index.php
+++ index.php
@@ -55,39 +55,7 @@
 			if($_SESSION["valid"] != true){
 				header("location: login.php");
 			}
-			# Get data by query
-			$query = 'select * from test_lab6';
-			$result = pg_query($pg_heroku, $query);
-			# Display data column by column
-			$i = 0;
-			echo '<html><body><table><tr>';
-			while ($i < pg_num_fields($result))
-			{
-				$fieldName = pg_field_name($result, $i);
-				echo '<td>' . $fieldName . '</td>';
-				$i = $i + 1;
-			}
-			echo '</tr>';
-			# Display data row by row
-			$i = 0;
-			while ($row = pg_fetch_row($result)) 
-			{
-				echo '<tr>';
-				$count = count($row);
-				$y = 0;
-				while ($y < $count)
-				{
-					$c_row = current($row);
-					echo '<td>' . $c_row . '</td>';
-					next($row);
-					$y = $y + 1;
-				}
-				echo '</tr>';
-				$i = $i + 1;
-			}
-			pg_free_result($result);
-
-			echo '</table></body></html>';
+			
 
 		?> 
 	</body>
