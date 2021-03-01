530876fe76d98736027e9d5ac826ae3f3097a92f
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 02:11:48 +0700

    as

--- index.php
+++ index.php
@@ -26,36 +26,17 @@
 					<li><a href="#">Danang store</a></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Change Password</a></li>
 					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
 				</ul>
 			 </div>
 		</nav>
-		<?php 
-			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
-			/*
-			# Heroku credential 
-			$host_heroku = "ec2-34-203-255-149.compute-1.amazonaws.com";
-			$db_heroku = "dfafdda85iuufp";
-			$user_heroku = "fwhvqbdasvcpdw";
-			$pw_heroku = "ee454b013d21e0981d72f13efd5297cb5392457328d856308fe8bd8b23ac168a";
-			
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			
-			$pg_heroku = pg_connect($conn_string);
-			
-			if (!$pg_heroku)
-			{
-				die('Error: Could not connect: ' . pg_last_error());
-			}
-			*/
-
+		<?php 			
 			session_start();
 			if($_SESSION["valid"] != true){
 				header("location: login.php");
 			}
-
+			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
 			# Display data column by column
 			$i = 0;
 			echo '<html><body><table><tr>';
@@ -86,7 +67,6 @@
 			pg_free_result($result);
 
 			echo '</table></body></html>';
-
 		?> 
 	</body>
 </html>
