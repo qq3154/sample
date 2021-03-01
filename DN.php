7cd6c82132100e955dbe4a9f76d049c05deb0a76
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 05:49:02 +0700

    a

--- DN.php
+++ DN.php
@@ -54,7 +54,7 @@
 			 </div>
 		</nav>
 
-		<button onclick="myFunction()">Try it</button>
+		<button onclick="myFunction()">Show products information</button>
 
 		<div id="myDIV">
 			<?php
@@ -99,9 +99,9 @@
 			function myFunction() {
 			  var x = document.getElementById("myDIV");
 			  if (x.style.display === "none") {
-				x.style.display = "block";
-			  } else {
 				x.style.display = "none";
+			  } else {
+				x.style.display = "block";
 			  }
 			}
 		</script>
