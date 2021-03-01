b44484802b7fc682111501c7b6b09ba95c1f7e30
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 00:16:09 +0700

    fix

--- connect.php
+++ connect.php
@@ -82,14 +82,7 @@
 	$query = 'select * from users';
 	$result = pg_query($pg_heroku, $query);
 	$row = pg_fetch_array($result);
-	if ($row['username']  == $username && $row['password'] == password)
-	{
-		echo "Login success!!!";
-	}
-	esle
-	{
-		echo "Failed to login!!!";
-	}
+	
 	
 ?>
 		
