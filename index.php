fb7c069b26a4ee08e88c9acc7868d449cf86682c
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 02:17:25 +0700

    asd

--- index.php
+++ index.php
@@ -37,16 +37,7 @@
 				header("location: login.php");
 			}
 			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
-			# Heroku credential 
-			$host_heroku = "ec2-34-203-255-149.compute-1.amazonaws.com";
-			$db_heroku = "dfafdda85iuufp";
-			$user_heroku = "fwhvqbdasvcpdw";
-			$pw_heroku = "ee454b013d21e0981d72f13efd5297cb5392457328d856308fe8bd8b23ac168a";
 			
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-				
-			$pg_heroku = pg_connect($conn_string);
 			# Get data by query
 			$query = 'select * from test_lab6';
 			$result = pg_query($pg_heroku, $query);
