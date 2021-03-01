8f7ba2bdd0cab067ae4412db597887019797df1e
Quân Nguyễn <kjbinben@gmail.com>
2021-03-01 22:34:44 +0700

    a

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
 					<li><a href="#">Danang store</a></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
 				</ul>
 			 </div>
 		</nav>
