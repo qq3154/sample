ef79c1bd2040111ffccfe742670ff22d0cff1d51
Quân Nguyễn <kjbinben@gmail.com>
2021-03-01 22:41:56 +0700

    a

--- connect.php
+++ connect.php
@@ -1,6 +1,8 @@
-$host_heroku = "ec2-34-203-255-149.compute-1.amazonaws.com";
+<?php
+	$host_heroku = "ec2-34-203-255-149.compute-1.amazonaws.com";
 			$db_heroku = "dfafdda85iuufp";
 			$user_heroku = "fwhvqbdasvcpdw";
 			$pw_heroku = "ee454b013d21e0981d72f13efd5297cb5392457328d856308fe8bd8b23ac168a";
-			# Create connection to Heroku Postgres
+	# Create connection to Heroku Postgres
 			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+?>
--- index.php
+++ index.php
@@ -39,9 +39,10 @@
 			$db_heroku = "dfafdda85iuufp";
 			$user_heroku = "fwhvqbdasvcpdw";
 			$pw_heroku = "ee454b013d21e0981d72f13efd5297cb5392457328d856308fe8bd8b23ac168a";
-			*/
+			
 			# Create connection to Heroku Postgres
 			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			*/
 			$pg_heroku = pg_connect($conn_string);
 			
 			if (!$pg_heroku)
