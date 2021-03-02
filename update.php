b5c403b958c9842d0bcb1fc47776d94c3e96455d
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 10:35:10 +0700

    v

--- update.php
+++ update.php
@@ -18,11 +18,12 @@
 			$name = $_POST['name'];
 			$price = $_POST['price'];
 			$quantity = $_POST['quantity'];	
-			$query = "UPDATE danang set product_price = $price, quantity = $quantity where product_name = '$name' ";
-			pg_query($pg_heroku, $query);	
 			$role =  $_SESSION["role"];
-			if($role == 2) echo "hanoi";
-			if($role == 3) echo "danang";
+			if($role == 2) $table = "hanoi";
+			if($role == 3) $table = "danang";
+			$query = "UPDATE $table set product_price = $price, quantity = $quantity where product_name = '$name' ";
+			pg_query($pg_heroku, $query);	
+			
 			echo "Update!!!";
 			header( "refresh:2;url=DN.php" );
 		} 
