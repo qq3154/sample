32cbbb55406ad92669c25fee3e7837c722f63d6d
Quân Nguyễn <kjbinben@gmail.com>
2021-03-02 09:05:26 +0700

    as

--- DN.php
+++ DN.php
@@ -154,24 +154,7 @@
 				
 			</div>
 
-			<div id="form">
-				<form action="connect.php" method="post">
-					<p>
-						<label>Username:</label>
-						<input type="text" placeholder="Enter Username" id="user" name="user"/>
-					</p>
 			
-					<p>
-						<label>Password:</label>
-						<input type="password" placeholder="Enter Password" id="pass" name="pass"/>
-					</p>
-			
-					<p>
-						<input type="submit" id="btn" value="Login"/>
-					</p>		
-			
-				</form>
-			</div>
 		</div>
 		
 		<script>
